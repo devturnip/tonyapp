de986848a2db7778708d0fac400a2d21fb14dd2f
devturnip <ossig.anthony@gmail.com>
2021-06-30 10:16:29 +0900

    Initial Commit

---
+++ tonyapp/tonyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tonyappApp.swift
+//  tonyapp
+//
+//  Created by Anthony Chua on 30/6/21.
+//
+
+import SwiftUI
+
+@main
+struct tonyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
